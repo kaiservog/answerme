fb74409bec4fc3e4b4169be75fc41ace8ad291c0
Kaiservog <kaiservog@gmail.com>
2016-01-20 02:25:07 -0200

    testes

--- server/src/main/java/com/server/main/MainTestCertificado.java
+++ server/src/main/java/com/server/main/MainTestCertificado.java
@@ -7,18 +7,23 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.jboss.weld.environment.se.events.ContainerInitialized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.server.conf.Configuration;
 
 public class MainTestCertificado {
+	private static final Logger logger = LoggerFactory.getLogger(MainTestCertificado.class);
+	
 	@Inject
 	private Configuration configuration;
 
 	public void main(@Observes ContainerInitialized event) {
 		if (configuration.isTLSEnabled()) {
 			secure("C:\\Users\\César\\Documents\\certs\\server.jks", "/852789@", null, null);
+			logger.info("TLS Enabled");
 		}
-		
+
 		get("/hello", (req, res) -> "Hello World");
 		System.out.println("teste");
 
