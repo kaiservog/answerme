29b23819f538011d8b57785a2f31d9b5e4e09a96
Kaiservog <kaiservog@gmail.com>
2016-01-24 22:43:41 -0200

    fb login

--- web/js/answeme.js
+++ web/js/answeme.js
@@ -2,18 +2,124 @@ angular.module('home', [])
     .controller('homeController', ['$scope', function($scope) {
         $scope.msgs = [
 			{
-				type: 'question',
-				topic: 'Java',
-				text: 'How to create classpath for maven?',
-				time: '15 mins ago',
-				avatar: 'http://placehold.it/50/55C1E7/fff'
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
 			},
 			{
-				type: 'answer',
-				topic: 'Java',
-				text: 'try MVN_HOME on control panel > add envirement variable',
-				time: '10 mins ago',
-				avatar: 'http://placehold.it/50/FA6F57/fff'
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
+			},
+			{
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
+			},
+			{
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
+			},
+			{
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
+			},
+			{
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
+			},
+			{
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
+			},
+			{
+				question: {
+					topic: 'Java',
+					text: 'How to create classpath for maven?',
+					time: '15 mins ago',
+					avatar: 'http://placehold.it/50/55C1E7/fff'
+				},
+				answer: {
+					text: 'try MVN_HOME on control panel > add envirement variable',
+					time: '10 mins ago',
+					avatar: 'http://placehold.it/50/FA6F57/fff',
+					status: 'Approved'
+				}
+				
 			}
 		];
     }]);
