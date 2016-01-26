ce6e330f7b1fdf5b017d12ca987032b8ce7b5f52
Kaiservog <kaiservog@gmail.com>
2016-01-26 01:26:13 -0200

    login com redirecionamento e sessao funfando

--- web/js/script-common.js
+++ web/js/script-common.js
@@ -1,6 +1,5 @@
 var answermeApp = angular.module('answermeApp', ['ngRoute']);
-
-answermeApp.factory('accountService', function() {
+answermeApp.service('accountService', function() {
 	 var savedData = {}
 	 function set(data) {
 	   savedData = data;
@@ -31,16 +30,70 @@ answermeApp.config(function($routeProvider, $locationProvider) {
 	});
 });
 
-answermeApp.controller('loginController', ['$scope', function($scope) {
-	$scope.accountHolder = accountService.set
+answermeApp.controller('loginController', ['$scope', 'accountService', '$location', function($scope, accountService, $location) {
+	$scope.statusChangeCallback = function (response) {
+		console.log('statusChangeCallback');
+		console.log(response);
+
+		if (response.status === 'connected') {
+		  console.log('Successful userId for: ' + response.authResponse.userID);
+		  console.log('Successful accessToken for: ' + response.authResponse.accessToken);
+		  
+		
+			
+		  var account = {
+			  userID :  response.authResponse.userID,
+			  accessToken : response.authResponse.accessToken,
+			  service : 'fb'
+		  }
+		  
+		FB.api('/me', function(response) {
+			account.name = response.name 
+		});
+		  
+		  accountService.set(account);
+		  $location.path("/home");
+		  $scope.$apply();
+		} else if (response.status === 'not_authorized') {
+		  document.getElementById('status').innerHTML = 'Please log into this app.';
+		} else {
+		  document.getElementById('status').innerHTML = 'Please log into Facebook.';
+		}
+	}
+	
+	$scope.checkLoginState = function () {
+		FB.getLoginStatus(function(response) {
+		  statusChangeCallback(response);
+		});
+	}
+	
+	window.fbAsyncInit = function() {
+	  FB.init({
+		appId      : '1682532568656067',
+		cookie     : true,  
+		xfbml      : true,  
+		version    : 'v2.2'
+	  });
+
+	  FB.getLoginStatus(function(response) {
+		$scope.statusChangeCallback(response);
+	  });
+	};
+
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
 }])
-.controller('MainCtrl', ['$route', '$routeParams', '$location',
-  function($route, $routeParams, $location) {
+.controller('MainCtrl', ['$route', '$routeParams', '$location', function($route, $routeParams, $location) {
     this.$route = $route;
     this.$location = $location;
     this.$routeParams = $routeParams;
 }])
-.controller('homeController', ['$scope', function($scope) {
+.controller('homeController', ['$scope', 'accountService', function($scope, accountService) {
 	$scope.accountHolder = accountService.get()
 	$scope.msgs = [
 		{
