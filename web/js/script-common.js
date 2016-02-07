0c0affc6a791dc528789caa34c68f7fe7c3d590c
Kaiservog <cesar.vog@gmail.com>
2016-02-07 01:24:05 -0200

    adicionado função de fazer questions

--- web/js/script-common.js
+++ web/js/script-common.js
@@ -182,19 +182,63 @@ answermeApp.controller('loginController', ['$scope', 'accountService', '$locatio
 	$scope.msgs = [];
 
 	$scope.delay = 5000;
+	$scope.alert = function(text, severity) {
+		var div = $('<div>').html(text);
+		div.addClass('alert');
+		div.addClass('alert-' + severity);
+		$('#messageContainer').append(div);
+		setTimeout(function() {
+			div.remove();	
+		}, 10000)
+	}
+
+	$scope.jumpTopic = function(event) {
+		if(event.which === 32) {
+			$('#question').focus();
+		}
+	}
+
+	$scope.ask = function() {
+		var askPackage = {
+			userId: $scope.accountHolder.userId,
+			token: $scope.accountHolder.token,
+			loginService: $scope.accountHolder.service,
+			topic: $('#topic').val().trim(),
+			question: $('#question').val().trim()
+		}
+
+		$http.defaults.headers.post['dataType'] = 'json'
+
+		$http({
+		  	method: 'POST',
+		  	headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+		  	url: 'https://localhost:4567/question/add',
+		  	dataType: 'json',
+		  	data: {request: askPackage}
+		  }).then(function(response) {
+		  	$('#topic').val('');
+		  	$('#question').val('');
+		  	$('#topic').focus();
+		  }, function(response){
+		  	console.log(response);
+		  	$scope.alert('Something weird happened', 'danger');
+		  });
+	} 
 
 	var checkId = setInterval(checkQuestions, $scope.delay);
 
 
 	function checkQuestions() {
 		var userId = $scope.accountHolder.userId;
+		var loginService = $scope.accountHolder.service;
+		var token = $scope.accountHolder.token;
 
 		$http.defaults.headers.post['dataType'] = 'json'
 
 		$http({
 		  	method: 'GET',
 		  	headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-		  	url: 'https://localhost:4567/question/find/' + userId + '/java',
+		  	url: 'https://localhost:4567/question/find/' + userId + '/' + token + '/' + loginService + '/java',
 		  	dataType: 'json'
 		  }).then(function(response) {
 		  		console.log('find');
