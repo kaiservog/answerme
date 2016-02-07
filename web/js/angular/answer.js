4ca9674a61dffd3695020b546c4d30c33d1738d8
Kaiservog <cesar.vog@gmail.com>
2016-02-07 18:23:21 -0200

    Adicionado contadores para respostas das perguntas

--- web/js/angular/answer.js
+++ web/js/angular/answer.js
@@ -1,14 +1,30 @@
 angular
 	.module('answermeApp')
-	.controller('answerController', ['$scope', '$http', '$location', 'accountService', 'questionService', 'accountService', function($scope, $http, $location, accountService, questionService, accountService) {
-		$scope.question = questionService.get();
-		$scope.accountHolder = accountService.get();
+	.controller('answerController', ['$scope', '$http', '$location', '$interval', 'accountService', 'questionService', 'accountService', 
+		function($scope, $http, $location, $interval, accountService, questionService, accountService) {
 		$scope.appendAnswer = function() {
-			$('#answer').removeClass('hidden');
+			$interval.cancel($scope.answerTimeout);
+			$('#answer').removeClass('ng-hide');
 			$('#bt-answer').addClass('hidden');
 			$('#bt-ignore').addClass('hidden');
+
+			$scope.countering = function() {
+				$scope.counter--;			
+			}
+			$scope.counteringId = $interval($scope.countering, 1000);
+
+			var timeout = setTimeout($scope.goHome, 15000);
 		}
+
 		$scope.goHome = function() {
+			if($scope.counteringId != undefined) $interval.cancel($scope.counterId);
 			$location.path('/home');
 		}
+
+		$scope.question = questionService.get();
+		$scope.accountHolder = accountService.get();
+
+		$scope.counter = 15;
+		$scope.answerTimeout = $interval($scope.goHome, 5000, 1);
+
 }]);
