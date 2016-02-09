03ee4d5d448fb0c143041319bb9af7042e099854
Kaiservog <cesar.vog@gmail.com>
2016-02-09 18:57:25 -0200

    Novo modal para mensagem respondida

--- web/js/angular/home.js
+++ web/js/angular/home.js
@@ -14,7 +14,7 @@ angular
 				div.remove();	
 			}, 10000)
 		}
-		$scope.alert('oi', 'info');
+		
 		$scope.messageContainerHider = function() {
 			var container = $('#messageContainer');
 			var containerBody = $('#messageContainer-body');
@@ -30,6 +30,10 @@ angular
 			if(event.which === 32) {
 				$('#question').focus();
 			}
+		}
+
+		$scope.updateModalShow = function() {
+			$('#update-modal').modal('show');
 		}
 
 		$scope.ask = function() {
@@ -58,6 +62,89 @@ angular
 			  	$scope.alert('Something weird happened', 'danger');
 			  });
 		} 
+
+		$scope.reject = 0;
+		$scope.directRejectAnswer = function () {
+			$scope.reject++;
+			if($scope.reject >= 2)  {
+				$('#answer-modal').modal('hide');
+			}
+		}
+
+		$scope.apply = function () {
+		var topics = $('#topics').val();
+
+		var request = {
+			userId: accountService.get().userId,
+			service: accountService.get().service,
+			topics: topics
+		}
+
+		$http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+		$http.defaults.headers.common['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE';
+		$http.defaults.headers.common['Access-Control-Max-Age'] = '3600';
+		$http.defaults.headers.common['Access-Control-Allow-Headers'] = 'x-requested-with';
+
+		$http.defaults.headers.post['dataType'] = 'json'
+
+		$http({
+		  	method: 'POST',
+		  	headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+		  	url: 'https://localhost:4567/user/update',
+		  	dataType: 'json',
+		  	data: {request: request}
+		  }).then(function(response) {
+		  	if(response.data.response.message=='ok') $location.path("/home");
+		  	}, function(response){console.log(response)});
+		}
+
+		$scope.workTopics = function (field) {
+			var val = $('#topics').val();
+
+			$scope.topics = splitTopics(val).map(mapTopics);
+		}
+
+		function mapTopics(element ) {
+			var letter = element.charAt(0);
+			color = colorTable[letter.toLowerCase()];
+			if(color == undefined) color = 'default';
+			var cElement =  element.charAt(0).toUpperCase() + element.slice(1);
+			return { text: cElement, color: color};
+		}
+
+		function splitTopics(rawTopics) {
+			var okRawTopics = rawTopics.replace("  ", " ");
+			var topics = okRawTopics.split(" ");
+			return topics;
+		}
+
+		var colorTable = {
+			a: 'default',
+			b: 'primary',
+			c: 'success',
+			d: 'info',
+			e: 'warning',
+			f: 'danger',
+			g: 'default',
+			h: 'primary',
+			i: 'success',
+			j: 'info',
+			k: 'warning',
+			l: 'danger',
+			m: 'warning',
+			n: 'primary',
+			o: 'success',
+			p: 'info',
+			q: 'danger',
+			r: 'info',
+			s: 'info',
+			t: 'danger',
+			u: 'success',
+			w: 'primary',
+			x: 'primary',
+			y: 'success',
+			z: 'info'
+		}
 
 		var checkId = setInterval(checkQuestions, $scope.delay);
 		var hiderId = setInterval($scope.messageContainerHider, 1000);
@@ -98,4 +185,5 @@ angular
 			  		
 			  	}, function(response){console.log(response)});
 		}
+		$("[data-toggle=popover]").popover();
 	}]);
